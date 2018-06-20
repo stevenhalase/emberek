7027688f3636338f8da3de10f9724eef68ba4202
Steven Halase <steven.halase@gmail.com>
2018-06-19 22:05:58 -0500

    Added express server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app); 
+
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/build'));
+
+server.listen(port, () => {
+    console.log('Server started at http://localhost:' + port);
+})
